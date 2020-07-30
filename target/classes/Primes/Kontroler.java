84ab3a85db10ba84837a5013a7656b6ea6d055d7
rastoliska25 <67071249+rastoliska25@users.noreply.github.com>
2020-07-30 11:17:34 +0200

    Start - version 5 problem fixed

--- target/classes/Primes/Kontroler.java
+++ target/classes/Primes/Kontroler.java
@@ -1,9 +1,21 @@
 package Primes;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 @RestController
 
 
 
 public class Kontroler {
+
+    private Random nahodne = new Random();
+    @GetMapping
+    public int nahodneCislo() {
+        return nahodne.nextInt();
+    }
+
+
+
 }
