5c5b2af95c113c10ff527ceeb86d7f516843a31f
rastoliska25 <67071249+rastoliska25@users.noreply.github.com>
2020-07-30 12:17:22 +0200

    Start - version 5 problem fixed

--- target/classes/Primes/Kontroler.java
+++ target/classes/Primes/Kontroler.java
@@ -1,5 +1,6 @@
 package Primes;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
@@ -11,11 +12,16 @@ import java.util.Random;
 public class Kontroler {
 
     private Random nahodne = new Random();
-    @GetMapping
+    @GetMapping("/random")
     public int nahodneCislo() {
         return nahodne.nextInt();
     }
 
 
+        @GetMapping(value = "/greet/{meno}")
+        public String greetUser(@PathVariable String meno) {
+            return "Ahoj, "+ meno;
+        }
+    }
 
-}
+
