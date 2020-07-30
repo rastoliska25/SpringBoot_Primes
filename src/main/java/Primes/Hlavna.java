b9084dd3389443997ff1de23f961144fcc4fe177
rastoliska25 <67071249+rastoliska25@users.noreply.github.com>
2020-07-30 10:32:59 +0200

    Start - version 5 problem fixed

---
+++ src/main/java/Primes/Hlavna.java
@@ -0,0 +1,15 @@
+package Primes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+
+public class Hlavna {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hlavna.class, args);
+
+
+    }
+}
